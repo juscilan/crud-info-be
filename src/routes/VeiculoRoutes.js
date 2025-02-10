6e3d7ce01a67d45f49e5dd1a63588d69c5308407
Juscilan Moreto <juscilan.moreto@xogito.com>
2025-02-10 14:37:16 -0300

    (info): changed routes file

--- src/routes/VeiculoRoutes.js
+++ src/routes/VeiculoRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const VeiculoController = require('../controllers/VeiculoController');
 const db = require('../../database/db');
+const VeiculoController = require('../controllers/VeiculoController');
 
 const router = express.Router();
 const veiculoController = new VeiculoController(db);
