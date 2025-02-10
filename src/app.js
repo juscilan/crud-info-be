e03432c078bf0d7707e532f3cc7cb790b3f81dbe
Juscilan Moreto <juscilan.moreto@xogito.com>
2025-02-10 16:17:01 -0300

    refactor: rename project to crud-info-be and add CORS support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const veiculoRoutes = require('./routes/VeiculoRoutes');
 
 const app = express();
+
+// Habilitando CORS para todas as origens (recomendado para desenvolvimento)
+// Agora todas as requisições de outros domínios são permitidas
+app.use(cors());  
+
 app.use(bodyParser.json());
 app.use(veiculoRoutes);
 
