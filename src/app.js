396b32db7ea3fb8a7358d805d725c9b4a8abf276
Juscilan Moreto <juscilan.moreto@xogito.com>
2025-02-10 21:15:07 -0300

    (info): added swagger

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
+const cors = require('cors');
+const path = require('path');
+const YAML = require('yamljs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const veiculoRoutes = require('./routes/VeiculoRoutes');
 
 const app = express();
 
 // Habilitando CORS para todas as origens (recomendado para desenvolvimento)
-// Agora todas as requisições de outros domínios são permitidas
 app.use(cors());  
+
+// Removendo o Powered-By - Boas Práticas de Segurança
+app.disable('x-powered-by');
+
+// Carregar o arquivo Swagger YAML
+
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+
+// Configurar o Swagger UI na rota /api-docs
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json());
 app.use(veiculoRoutes);
 
 const PORT = 4000;
+
 app.listen(PORT, () => {
   console.log(`Server Running on port: ${PORT}`);
 });
